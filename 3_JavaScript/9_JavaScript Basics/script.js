aa7b6bca019d8d108b36a1627baca306e7fea06b
Himanshu <himanshuramchandani08@gmail.com>
2022-03-24 18:50:38 +0530

    Day 9 JavaScript Basics

---
+++ 3_JavaScript/9_JavaScript Basics/script.js
@@ -0,0 +1,61 @@
+alert("Hello from external JavaScript");
+
+// variables
+
+var a = 10;
+
+console.log(a);
+
+a = 20;
+
+console.log(a);
+
+console.log(a + 5);
+
+console.log(a);
+
+// display values in HTML
+
+var x = 100;
+
+document.getElementById('one').innerHTML = x;
+
+
+document.getElementById('two').innerHTML = "Hello Abhishek";
+
+
+// operators
+
+// unary operators
+// ++ --
+
+var c = 10;
+c++;
+console.log(c);
+
+// 1. Arithmatic Operator
+// + - * / % 
+
+// Relational Operators
+// > < >= <= == != ===
+
+// boolean operators
+// true false
+
+// logical
+// &&  ||  ^
+
+// Algorithm Based Questions
+
+// 1
+
+// 1  -  10   --> 3  5  6  9   - sum -> 23
+
+// 1 - 1000  -->  3 or 5  --> 
+//  15  X
+
+
+
+
+
+
