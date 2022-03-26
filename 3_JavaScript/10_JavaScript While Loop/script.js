10a32e2d843f53d6bcc4c878894453b6ee88df70
Himanshu <himanshuramchandani08@gmail.com>
2022-03-26 18:45:27 +0530

    Day 10 JavaScrip While loop

---
+++ 3_JavaScript/10_JavaScript While Loop/script.js
@@ -0,0 +1,97 @@
+// while loop
+
+
+// var i = 0;
+
+// while (i < 5){
+//     console.log("MERN");
+//     i++;  // i = i + 1
+// }
+
+// var i = 0;
+
+// while (i < 5){
+//     console.log(i);
+//     i++;  // i = i + 1
+// }
+
+
+// var x = "";
+// var i = 0;
+// while (i < 5){
+//     x = x + i + "<br>";  // concatination
+//     i++;
+// }
+// document.getElementById("four").innerHTML = x;
+
+
+// user input
+// n = prompt("Enter a number");
+
+// alert(n);
+
+// n = prompt("Enter Your Name");
+
+// alert("Hello " +n);
+
+// floor division
+
+// 153 % 10 - 3
+// 153 // 10 - 15
+
+// 15 % 10 - 5
+// 15 // 10 - 1
+
+// 1 % 10 - 1
+// 1 // 10 - 0
+
+// x = Math.floor(153 / 10)
+// console.log(x);
+
+
+// wap to enter a number from user and 
+// print reverse of it
+
+// var c = prompt("Enter a number: ");
+// var r;
+// var s = "";
+
+// while(c > 0){
+//     r = c % 10;
+//     s = s + r;
+//     c = Math.floor(c/10);
+// }
+
+// console.log(s);
+
+
+// wap to enter a number from user and 
+// print sum of its individual digits.
+
+
+// wap to enter a number form user and 
+// print factorial of it.
+
+
+// type of
+
+console.log(typeof 10);  // number
+console.log(typeof "10"); // string
+
+
+console.log(typeof 10.5);  // number
+console.log(typeof 'A');  // string
+
+console.log(typeof 'India');  // string  
+console.log(typeof 0);       // number
+
+console.log(typeof 1);       // number
+console.log(typeof true);    // boolean
+
+
+
+//   2520   --> 1 - 10
+
+//    ?     --> 1 - 20
+
+
