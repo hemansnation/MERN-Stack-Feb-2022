c91f796b066b2b17de77e3bbe0f569133a834283
Himanshu <himanshuramchandani08@gmail.com>
2022-03-29 19:02:25 +0530

    Day 11 JavaScript for loop and do while

---
+++ 3_JavaScript/11_JavaScript For loop/script.js
@@ -0,0 +1,65 @@
+// while
+
+// wap to enter a number from 
+// user and print sum of its individual digits.
+
+// 153  -   9
+
+// var n = prompt("Enter a number: ");
+// var s = 0;
+// var r;
+
+// while(n > 0){
+//     r = n%10;
+//     s = s + r;
+//     n = Math.floor(n / 10);
+// }
+// console.log(s);
+// document.getElementById('four').innerHTML = s;
+
+
+// do while 
+
+// var i = 1;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 10);
+
+
+// for loop
+
+// for(var a = 1 ; a <= 10 ; a++ ){
+//     console.log(a);
+// }
+
+// for in loop
+
+var arr = [10,20,30,40,50];
+//          0  1  2  3  4
+//                      j
+
+for (j in arr){
+    console.log(j);
+}
+
+console.log(j);  // last value of the previous loop
+
+for (k in arr){
+    console.log(arr[k]);
+}
+
+// wap to take a array, and print even indexed values of it 
+// using for in loop.
+
+
+// wap to summ all the values of an array using for in loop
+
+
+
+// biggest 2 digit  - 91 * 99
+// multiplication  - 9009
+
+// biggest   3 digit  ??
+// multiplication is a pallindrome
