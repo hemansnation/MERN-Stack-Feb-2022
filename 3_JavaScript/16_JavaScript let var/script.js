b7e48db70aefd1a3edb662619151d93c3487dc3c
Himanshu <himanshuramchandani08@gmail.com>
2022-04-16 19:05:09 +0530

    Day 16 JavaScript Var let objects

---
+++ 3_JavaScript/16_JavaScript let var/script.js
@@ -0,0 +1,98 @@
+var g = "Hello World";
+var g = "Indore";
+
+console.log(g);
+
+
+// var x = "India";   // global variable
+
+// function goa(){
+//     var y = "Bye";   // local variable
+//     console.log(y);
+//     console.log(x);
+// }
+
+// goa();
+
+// console.log(y);
+
+
+// function indore(){
+//     a = "hello hi";    // global variable
+
+//     console.log(a);
+//     var h = "goa";
+//     console.log(h);
+
+// }
+// indore();
+// console.log(a);
+
+
+// var a = "hi this is javascript";
+
+// var time = 5;
+
+// if(time > 4){
+//     var a = "hello";
+// }
+
+// console.log(a);
+
+// let 
+
+// let cannot be redeclared
+// blocked scope
+
+// let d = "hi";
+// let d = "bye";
+
+// console.log(d);
+
+let r = 10;
+let s = 20;
+
+{
+    let r = 100;
+    console.log(r);
+}
+
+r = 40;
+console.log(r);
+
+let hello;
+console.log(hello);
+
+
+var hi;
+console.log(hi);
+
+
+// TV
+// smart
+// bluetooth
+// hdmi
+// wifi
+
+
+
+// Actual TV
+// product ID
+
+
+// remote
+
+// JSON - JavaScript Object Notation
+
+let anurag = {
+    name: "Anurag Kashyap",
+    age: 21,
+    college: 'Medicaps'
+}
+
+console.log(anurag);
+console.log(anurag.name);
+console.log(anurag.age);
+
+console.log(anurag['college']);
+console.log(anurag['age']);
